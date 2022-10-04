6a252af38b9a52f36ec45618258e638efecc1ceb
carnoldcoding <carnoldcoding@gmail.com>
2022-10-04 13:34:34 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import myName from "./myName";
+import './styles.css';
+import Profile from './assets/profile.jpeg'
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, is imported above
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add("hello");
+
+    // Add the image to our existing div.
+    const myProfile = new Image();
+    myProfile.src = Profile;
+    element.appendChild(myProfile);
   
     return element;
   }
